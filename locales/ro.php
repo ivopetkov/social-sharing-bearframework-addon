b9a80a15eaec6b2cdcb92f90ee836427557dfd8a
Ivo Petkov <ivo@ivopetkov.com>
2026-02-28 21:09:42 +0200

    Add Romanian locale.

---
+++ locales/ro.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'ivopetkov.socialSharing.Share' => 'Distribuie',
+    'ivopetkov.socialSharing.ShareOnFacebook' => 'Distribuie pe Facebook',
+    'ivopetkov.socialSharing.ShareOnTwitter' => 'Distribuie pe X (Twitter)',
+    'ivopetkov.socialSharing.ShareOnLinkedIn' => 'Distribuie pe LinkedIn',
+    'ivopetkov.socialSharing.ShareInAnEmail' => 'Trimite prin email',
+    'ivopetkov.socialSharing.CopyURL' => 'Copiază URL',
+    'ivopetkov.socialSharing.CopyURLDone' => 'URL-ul a fost copiat',
+    'ivopetkov.socialSharing.DeviceShare' => 'Alte opțiuni',
+];
